41d04d1e16b5f06074410a223dc358b3b97eabca
haoxl3 <1098262516@qq.com>
2017-12-22 17:38:11 +0800

    footer component

---
+++ components/mFooter.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="footer-container">
+        <ul class="clearfix">
+            <li class="car-position">
+                <i class="iconfont">&#xe642;</i>
+                <span>你的爱车停放在燕东大厦科技园2号楼3号停车场</span>
+            </li>
+            <li>
+                <i class="iconfont icon-weather">&#xe767;</i>
+                <span>21度</span>
+            </li>
+            <li>今日不限行</li>
+        </ul>
+    </div>
+</template>
+<style lang="stylus" scoped>
+    .footer-container{
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        padding: 14px 10px 8px;
+        background-color: #fff;
+        box-sizing: border-box;
+        ul{
+            
+            height: 2rem;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+            padding: 6px 4px;
+            box-sizing: border-box;
+            display: flex;
+            li{
+                flex: 2;
+                text-align: center;
+                i{
+                    vertical-align: middle;
+                    margin-right: 4px;
+                    color: #666;
+                }
+                .icon-weather{
+                    color: #FA9D26;
+                }
+            }
+            .car-position{
+                flex: 4;
+                text-align: left;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                display: -webkit-box;
+                -webkit-line-clamp: 1;
+                -webkit-box-orient: vertical;
+            }
+            li:last-child{
+                border-left: 1px solid #ccc;
+            }
+        }
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,65 @@
 <template>
   <div>
     <m-header :isReturn="false"></m-header>
+    <section class="top-banner">
+      <div class="avatar-box">
+        <div class="avatar"><i class="iconfont"></i></div>
+        <p>安然</p>
+      </div>
+      <i class="iconfont setting">&#xe615;</i>
+    </section>
     <div>
       <nuxt-link to="/home/pay">自助交费</nuxt-link>
       <nuxt-link to="/home/appoint">预约车位</nuxt-link>
     </div>
+    <m-footer></m-footer>
   </div> 
 </template>
 
 <script>
 import mHeader from '~/components/mHeader.vue'
+import mFooter from '~/components/mFooter.vue'
+
 export default {
   components: {
-    mHeader
+    mHeader,
+    mFooter
   }
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
+.top-banner{
+  height: 8rem;
+  background-color: #fff;
+  margin-bottom: 8px;
+  position: relative;
+  .avatar-box{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    .avatar{
+      width: 4rem;
+      height: 4rem;
+      text-align: center;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      background: url('~/static/imgs/avatar.jpeg') no-repeat center center;
+      background-size: cover;
+    }
+    p{
+      text-align: center;
+      color: #666;
+      margin-top: 0.6rem;
+    }
+  }
+  .setting{
+    font-size: 1.4rem;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    color: #888;
+  }
+}
 </style>
