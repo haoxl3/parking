c6924a3477b25c5db66e90cc7c392698a916a821
haoxl3 <1098262516@qq.com>
2018-01-10 14:39:03 +0800

    near page

--- pages/home/near.vue
+++ pages/home/near.vue
@@ -1,11 +1,30 @@
 <template>
+  <div>
     <m-header><span>附近</span></m-header>
+    <div class="map"></div>
+    <div class="near-info-box">
+      <park-space-info></park-space-info>
+      <park-space-info></park-space-info>
+    </div>
+  </div>
 </template>
 <script>
-import mHeader from '~/components/mHeader.vue'
+import mHeader from '~/components/mHeader'
+import parkSpaceInfo from '~/components/parkSpaceInfo'
+
 export default {
   components: {
-    mHeader
+    mHeader,
+    parkSpaceInfo
   }
 }
 </script>
+<style lang="stylus">
+.map{
+  height: 200px;
+  background-color: pink;
+}
+.near-info-box{
+  background-color: #fff;
+}
+</style>
